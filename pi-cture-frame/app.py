6fb5bd9cc3f01d297d19a9653a6949b749f1b083
John Wright <jwright6323@gmail.com>
2015-12-23 09:50:56 +0000

    displaying a photo

--- pi-cture-frame/app.py
+++ pi-cture-frame/app.py
@@ -3,6 +3,7 @@
 
 import os
 import Tkinter as tk
+from PIL import Image,ImageTk
 
 class Application(tk.Frame):
 
@@ -11,14 +12,13 @@ class Application(tk.Frame):
     w, h = root.winfo_screenwidth(), root.winfo_screenheight()
     root.overrideredirect(1)
     root.geometry("%dx%d+0+0" % (w,h))
+    root.title('Pi-cture Frame')
     tk.Frame.__init__(self, root)
     self.grid()
-    self.createWidgets()
-
-  def createWidgets(self):
-    self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-    self.quitButton.grid()
+    im = ImageTk.PhotoImage(Image.open("/tmp/pi-cture-frame/23622236440.jpg").crop((0,0,1024,800)))
+    panel = tk.Label(root, image = im)
+    panel.image = im
+    panel.pack(side = "bottom", fill="both", expand="yes")
 
 app = Application()
-app.master.title('Sample application')
 app.mainloop()
